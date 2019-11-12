3284cd36c04644b3d79fa3546c23f5d868dbd17e
LeviSmithCodes <eyevel@gmail.com>
2019-11-12 13:33:30 -0700

    finished strings.js

--- app/strings.js
+++ app/strings.js
@@ -1,12 +1,23 @@
 // Write a function that returns the character from a string at a given position
 function letterAtPosition(str, position) {
-
+  return str[position];
 }
 
-// Write a function that will join two strings with an optional seperating character 
+// let myLetter = letterAtPosition("Levi", 1);
+// console.log(myLetter);
+
+// Write a function that will join two strings with an optional seperating character
 function concat(str1, str2, seperator = " ") {
+  if (seperator) {
+    return str1 + seperator + str2;
+  } else {
+    return str1 + str2;
+  }
 }
 
 // Write a function that will capitalize the letter at a given position and return it
 function capitalizer(str, position) {
+  myArr = str.split("");
+  myArr[position] = myArr[position].toUpperCase();
+  return myArr[position];
 }
